9c22804690a3ba16854e116cc04491aff76aca4a
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-09-09 13:04:29 +0100

    working with Switch and exact on routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+
 import Homepage from './pages/homepage/homepage.components';
+
+const TopsPage = () =>(
+  <div>
+    <h1>This is the Tops Page</h1>
+  </div>
+);
+
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+      <Route  path='/' component={Homepage}/>
+      <Route  path='/tops' component={TopsPage}/>
+      {/*exact={true} means go to the exact url location*/}
+      {/*<Switch> means, render if ther is a match*/}
+      </Switch>
     </div>
   );
 }
+
 
 export default App;

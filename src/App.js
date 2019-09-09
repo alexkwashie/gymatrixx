d12f3c059a6316a2ba809e46a1ead33828dd08ba
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-09-09 14:15:44 +0100

    passing in unique identifiers

--- src/App.js
+++ src/App.js
@@ -10,13 +10,27 @@ const TopsPage = () =>(
   </div>
 );
 
+const Fleece = () =>(
+  <div>
+    <h1>This is the Tops/Fleece page</h1>
+  </div>
+);
+
+const RedFleece = (props) =>(
+  <div>
+    <h1>This is the red fleece page : {props.match.params.RedFleeceid}</h1> {/*this is to pass in a unique id from a database*/}
+  </div>
+);
+
 
 function App() {
   return (
     <div>
       <Switch>
-      <Route  path='/' component={Homepage}/>
-      <Route  path='/tops' component={TopsPage}/>
+      <Route  exact path='/' component={Homepage}/>
+      <Route  exact path='/tops' component={TopsPage}/>
+      <Route  exact path='/tops/fleece' component={Fleece}/>
+      <Route  path='/tops/fleece/:RedFleeceid' component={RedFleece}/>
       {/*exact={true} means go to the exact url location*/}
       {/*<Switch> means, render if ther is a match*/}
       </Switch>

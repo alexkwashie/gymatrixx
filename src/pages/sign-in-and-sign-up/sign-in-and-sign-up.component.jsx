c494a5c344324ef0192bbdb97d83e9d54d0ab848
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-09-11 15:27:20 +0100

    Create pages for Signing in

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+ import React from 'react';
+
+ import './sign-in-and-sign-up.style.scss'
+
+const SignInandSingUp = () =>(
+    <div className='sign-in-and-sign-up'>
+        <h1>Sign In</h1>
+    </div>
+)
+
+
+export default SignInandSingUp;

871c7c2764e179612b03e11373c85ee407862610
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-09-11 14:27:01 +0100

    adding sytling to header and app.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
 import shopPage from './pages/shop/shop.component'
 
-
 import Homepage from './pages/homepage/homepage.components';
-
+import Header from './components/header/header.component'
 
 function App() {
     return (
         <div>
-
+            <Header />
+            <Switch>
             <Route exact path='/' component={Homepage}/>
             <Route path='/shop' component={shopPage}/>
             {/*exact={true} means go to the exact url location*/}
             {/*<Switch> means, render if there is a match*/}
             {/*<Link to='/tops'>Tops</Link> */}
-
+            </Switch>
         </div>
     );
 }

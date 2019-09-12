4d4141f5bfa74c674cc50338415bd637f11a8a13
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-09-12 23:10:05 +0100

    getting current user details

--- src/App.js
+++ src/App.js
@@ -6,21 +6,44 @@ import shopPage from './pages/shop/shop.component'
 import Homepage from './pages/homepage/homepage.components';
 import Header from './components/header/header.component'
 import SignInandSingUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {auth} from './firebase/firebase.utils'
 
-function App() {
-    return (
-        <div>
-            <Header/>
-            <Switch>
-            <Route exact path='/' component={Homepage}/>
-            <Route path='/shop' component={shopPage}/>
-            <Route path='/signin' component={SignInandSingUp}/>
-            {/*exact={true} means go to the exact url location*/}
-            {/*<Switch> means, render if there is a match*/}
-            {/*<Link to='/tops'>Tops</Link> */}
-            </Switch>
-        </div>
-    );
+class App extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    //when page mounts this assigns logged user details to currentUser
+    componentDidMount(){
+        auth.onAuthStateChanged(user => {
+            this.setState = ({currentUser: user});
+
+            console.log(user);
+
+
+        })
+    }
+
+        render(){
+            return (
+                <div>
+                    <Header/>
+                    <Switch>
+                    <Route exact path='/' component={Homepage}/>
+                    <Route path='/shop' component={shopPage}/>
+                    <Route path='/signin' component={SignInandSingUp}/>
+                    {/*exact={true} means go to the exact url location*/}
+                    {/*<Switch> means, render if there is a match*/}
+                    {/*<Link to='/tops'>Tops</Link> */}
+                    </Switch>
+                </div>
+            );
+        }
+
 }
 
 export default App;

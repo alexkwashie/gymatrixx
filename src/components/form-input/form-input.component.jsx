99c1659e249ffbad49765a4ef47ee46af3f8c2e6
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-09-12 11:49:01 +0100

    adding styling to form input before adding it to sign-in comp

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './form-input.style.scss'
 
-const FormInput =()=>(
+const FormInput =({handleChange, label, ...otherProps})=>(
+    <div className='group'>
+        <input className = 'form-input' onChange={handleChange} {...otherProps}/>
 
+        //code below: if there is a label, apply class shrink when the useclicks into input field
+        //apply passed in label to the label class
+        {
+            label ?
+            (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+                {label}
+            </label>
+
+            )
+            : null
+        }
+    </div>
 
 )
 

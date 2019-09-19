6a813efd153e60242e108c1f2a429fb5fe053361
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-09-19 13:21:20 +0100

    adding Provider to the index.js file, and creating reducer folders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+//this provider object allows us to access items from the redux file in the main app below
+
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+    <Provider>
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+    </Provider>,
+
+    document.getElementById('root'));

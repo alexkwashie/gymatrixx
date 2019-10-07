a8ef141e061893cb066747bc45e2b1e0b8a35bc8
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-10-07 15:44:21 +0100

    Creating and adding custom buttoms to collection images, changing document title

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ({id,name,price,imageUrl}) =>(
     <div className='collection-items'>
@@ -10,7 +11,7 @@ const CollectionItem = ({id,name,price,imageUrl}) =>(
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-
+<CustomButton inverted> Add to Cart </CustomButton> {/*add custom button with the props*/}
     </div>
 )
 
